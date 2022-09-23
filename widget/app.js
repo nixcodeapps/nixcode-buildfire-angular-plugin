b00511206ab51a1fbf2ef0916289e2ea4f80d36e
Masresha <masresha.tsegaye@gmail.com>
2022-09-23 17:59:15 +0300

    changed some features

--- widget/app.js
+++ widget/app.js
@@ -86,6 +86,22 @@
             //     });
             // }
         }])
+        .directive('copyToClipboard', function () {
+            return {
+                restrict: 'A',
+                link: function (scope, elem, attrs) {
+                    elem.click(function () {
+                        if (attrs.copyToClipboard) {
+                            var $temp_input = $("<input>");
+                            $("body").append($temp_input);
+                            $temp_input.val(attrs.copyToClipboard).select();
+                            document.execCommand("copy");
+                            $temp_input.remove();
+                        }
+                    });
+                }
+            };
+        })
         .directive('ngCompare', function () {
             return {
             require: 'ngModel',
